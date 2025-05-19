9bdd5da207c98ce8cf5088e21926f4bb85b3f587
Éverton M. Vieira <emuvi@outlook.com.br>
2025-05-18 21:53:58 -0300

    updates

--- src/main/java/br/com/pointel/charvs/CharvsDesk.java
+++ src/main/java/br/com/pointel/charvs/CharvsDesk.java
@@ -244,7 +244,11 @@ public class CharvsDesk extends javax.swing.JFrame {
             var folder = new File(fieldOrigin.getText());
             var file = new File(folder, comboOrigin.getSelectedItem().toString());
             var origin = Files.readString(file.toPath());
-            origin = origin.replace("<INSERT>", body);
+            if (origin.contains("<INSERT>")) {
+                origin = origin.replace("<INSERT>", body);
+            } else {
+                origin = origin + "\n\n" + body;
+            }
             WizDesk.putStringOnClipboard(origin);
         } catch (Exception e) {
             WizDesk.showError(e);
