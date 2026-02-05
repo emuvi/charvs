cb85f1f8f19a2361596b73b842d9316f8b5bb7c2
Éverton M. Vieira <emuvi@outlook.com.br>
2026-02-05 12:07:59 -0300

    feat: new setup names

--- src/main/java/br/com/pointel/charvs/Setup.java
+++ src/main/java/br/com/pointel/charvs/Setup.java
@@ -5,67 +5,67 @@ import br.com.pointel.jarch.mage.WizString;
 
 public class Setup {
     
-    public static final String keyOnNewClipboardText = "FRAME_SETUP_COMP_ONCLIPBOADNEWTEXT";
+    public static final String keyOnNewClipboardText = "FRAME_SETUP_COMP_ON_CLIPBOARD_NEW_TEXT";
     
     public static OnNewClipboardText getOnNewClipboardText() {
         return OnNewClipboardText.values()[WizProps.get(keyOnNewClipboardText, 0)];
     }
 
-    public static final String keyOnNaming = "FRAME_SETUP_COMP_ONNAMING";
+    public static final String keyOnNaming = "FRAME_SETUP_COMP_ON_NAMING";
     
     public static OnNaming getOnNaming() {
         return OnNaming.values()[WizProps.get(keyOnNaming, 0)];
     }
 
-    public static final String keyNameNumberedPrefix = "FRAME_SETUP_COMP_NAMENUMBEREDPREFIX";
+    public static final String keyNameNumberedPrefix = "FRAME_SETUP_COMP_NAME_NUMBERED_PREFIX";
     
     public static String getNameNumberedPrefix() {
         return WizProps.get(keyNameNumberedPrefix, "");
     }
 
-    public static final String keyNameNumberedSize = "FRAME_SETUP_COMP_NAMENUMBEREDSIZE";
+    public static final String keyNameNumberedSize = "FRAME_SETUP_COMP_NAME_NUMBERED_SIZE";
     
     public static Integer getNameNumberedSize() {
         return WizProps.get(keyNameNumberedSize, 0);
     }
 
-    public static final String keyNameNumberedSuffix = "FRAME_SETUP_COMP_NAMENUMBEREDSUFFIX";
+    public static final String keyNameNumberedSuffix = "FRAME_SETUP_COMP_NAME_NUMBERED_SUFFIX";
     
     public static String getNameNumberedSuffix() {
         return WizProps.get(keyNameNumberedSuffix, "");
     }
 
-    public static final String keyStripFirstLines = "FRAME_SETUP_COMP_STRIPFIRSTLINES";
+    public static final String keyStripFirstLines = "FRAME_SETUP_COMP_STRIP_FIRST_LINES";
     
     public static Integer getStripFirstLines() {
         return WizProps.get(keyStripFirstLines, 0);
     }
 
-    public static final String keyNameExtension = "FRAME_SETUP_COMP_NAMEEXTENSION";
+    public static final String keyNameExtension = "FRAME_SETUP_COMP_NAME_EXTENSION";
     
     public static String getNameExtension() {
         return WizString.firstNonEmpty(WizProps.get(keyNameExtension, ""), ".txt");
     }
     
-    public static final String keyOnSave = "FRAME_SETUP_COMP_ONSAVE";
+    public static final String keyOnSave = "FRAME_SETUP_COMP_ON_SAVE";
     
     public static OnSave getOnSave() {
         return OnSave.values()[WizProps.get(keyOnSave, 0)];
     }
     
-    public static final String keyOnRecord = "FRAME_SETUP_COMP_ONRECORD";
+    public static final String keyOnRecord = "FRAME_SETUP_COMP_ON_RECORD";
     
     public static OnRecord getOnRecord() {
         return OnRecord.values()[WizProps.get(keyOnRecord, 0)];
     }
 
-    public static final String keyRecordPrefix = "FRAME_SETUP_COMP_RECORDPREFIX";
+    public static final String keyRecordPrefix = "FRAME_SETUP_COMP_RECORD_PREFIX";
     
     public static String getRecordPrefix() {
         return WizProps.get(keyRecordPrefix, "");
     }
 
-    public static final String keyRecordSuffix = "FRAME_SETUP_COMP_RECORDSUFFIX";
+    public static final String keyRecordSuffix = "FRAME_SETUP_COMP_RECORD_SUFFIX";
     
     public static String getRecordSuffix() {
         return WizProps.get(keyRecordSuffix, "");
