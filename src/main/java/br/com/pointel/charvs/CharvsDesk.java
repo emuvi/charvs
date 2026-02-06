c92813a3632c4e6689dc38102060f9c6d3dda48b
Éverton M. Vieira <emuvi@outlook.com.br>
2026-02-06 15:49:11 -0300

    feat: capitalize only with all on upper case

--- src/main/java/br/com/pointel/charvs/CharvsDesk.java
+++ src/main/java/br/com/pointel/charvs/CharvsDesk.java
@@ -343,7 +343,7 @@ public class CharvsDesk extends DFrame {
 
     private String cleanFileName(String title) {
         title = title.trim();
-        if (title.equals(title.toUpperCase()) || title.equals(title.toLowerCase())) {
+        if (title.equals(title.toUpperCase()))) {
             title = WizString.capitalizeWords(title.toLowerCase());
         }
         title = title
